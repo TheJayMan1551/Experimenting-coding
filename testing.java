faac91e57aa24d653705af704dfc038a496612dc
Jason Montalvo <jasonkaka578@icloud.com>
2018-02-01 13:53:07 -0800

    added a conditional statement
    asked  the user for a number and I squared it and depending on the conditional statement, it will print out one of the two statements.

--- testing.java
+++ testing.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
 public class testing {
-  
+
   public static void main(String[] args) {
-    
+
     Scanner input = new Scanner(System.in);
-    
+
     int numSquare = 0;
-    
+
     System.out.print("Enter a number and I will square it for you: ");
     int num = input.nextInt();
     numSquare = num * num;
-    
+
     System.out.println(numSquare);
-    
+
+    if (numSquare <= 50) {
+      System.out.print("That is a small number");
+    } else {
+      System.out.println("That is a big number");
+    }
+
   }
-  
-  
+
+
 }
