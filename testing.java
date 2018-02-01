69d51f0829faf2b1a4a3dc7a8806cb8e8f372224
Jason Montalvo <jasonkaka578@icloud.com>
2018-02-01 13:43:29 -0800

    Update testing.java

--- testing.java
+++ testing.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 public class testing {
   
   public static void main(String[] args) {
     
-    System.out.println("Hello World");
+    Scanner input = new Scanner(System.in);
     
+    int numSquare = 0;
+    
+    System.out.print("Enter a number and I will square it for you: ");
+    int num = input.nextInt();
+    numSquare = num * num;
+    
+    System.out.println(numSquare);
     
   }
   
