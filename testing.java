1eb4df97d7802493924f6a0ef1da246e3b6268ce
Jason Montalvo <jasonkaka578@icloud.com>
2018-02-02 11:47:36 -0800

    More changes...
    added another question asking the user whether or not they wanted to see the final result.

--- testing.java
+++ testing.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 public class testing {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
-    int numSquare = 0;
+		int numSquare = 0;
 
-    System.out.print("Enter a number and I will square it for you: ");
-    int num = input.nextInt();
-    numSquare = num * num;
+		System.out.print("Enter a number and I will square it for you: ");
+		int num = input.nextInt();
+		numSquare = num * num;
 
-    System.out.println(numSquare);
+		System.out.println(numSquare);
 
-    if (numSquare <= 50) {
-      System.out.print("That is a small number");
-    } else {
-      System.out.println("That is a big number");
-    }
+		System.out.println("Would you like to see the results?(1 for yes)");
+		int answer = input.nextInt();
 
-  }
+		if (answer == 1) {
+			if (numSquare <= 50) {
+				System.out.print("That is a small number");
+			} else {
+				System.out.println("That is a big number");
+			}
+		}
+	}
 
 
 }
